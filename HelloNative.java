0bd24f1ffdfe9a156abbb777ab746ebf4e0fd8cf
季洪安 <819342089@qq.com>
2021-06-10 13:56:16 +0800

    第一次提交代码

---
+++ HelloNative.java
@@ -0,0 +1,12 @@
+public class HelloNative {
+    static {
+        System.loadLibrary("HelloNative");
+    }
+
+    public static native void sayHello();
+    
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
